1361df39695c9fcec06a9ad038a7266e1470e819
Maninderjit Singh <sukhisembhy@hotmail.com>
2017-04-14 15:55:19 -0700

    changin mine

--- db/schema.rb
+++ db/schema.rb
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 20170407230958) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "firstName"
+    t.string   "lastName"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -47,8 +49,6 @@ ActiveRecord::Schema.define(version: 20170407230958) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "firstName"
-    t.string   "lastName"
     t.string   "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
