0dc17dfe1e2132a9f321fcbfe35b2144637f3d31
akshat46 <akshatsharma46@gmail.com>
2017-04-18 12:52:52 -0700

    working geocoder,maps-API;fixed navbar;formating input fields

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,30 @@ class Post < ApplicationRecord
 
 	accepts_nested_attributes_for :addresses, 
 	:allow_destroy => true, :reject_if => :all_blank
+
+
+  def set_iframe_src
+    main_string = "https://www.google.com/maps/embed/v1/directions?key=AIzaSyD1zV2b2rTtvUYLSOL9CiNxTKiB2hMBeCI"
+    origin_tag = "&origin="
+    destination_tag = "&destination="
+    origin_coord = ""
+    destination_coord = ""
+
+    self.addresses.each_with_index do |address, index|
+      coordinates = address.latitude.to_s + "," + address.longitude.to_s
+
+      if index==0
+        origin_coord = address.street.to_s + "+" + address.city.to_s + "+" + address.state.to_s
+      end
+
+      if index==1
+        destination_coord = address.street.to_s + "+" + address.city.to_s + "+" + address.state.to_s
+      end
+    end
+
+    origin_tag = origin_tag + origin_coord
+    destination_tag = destination_tag + destination_coord
+    main_string = main_string + origin_tag + destination_tag
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170407230958) do
+ActiveRecord::Schema.define(version: 20170416184720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20170407230958) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "post_id"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "posts", force: :cascade do |t|
